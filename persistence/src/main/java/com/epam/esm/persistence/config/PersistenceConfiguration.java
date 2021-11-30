e890ee4dec739e0522a62b35aa7e78f801843bb1
Anton Radzionau <radionoff.antony@gmail.com>
2021-11-30 16:14:33 +0300

    up! persistConfig has been added.

---
+++ persistence/src/main/java/com/epam/esm/persistence/config/PersistenceConfiguration.java
@@ -0,0 +1,49 @@
+package com.epam.esm.persistence.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.epam.esm")
+@PropertySource("classpath:database/db.properties")
+public class PersistenceConfiguration {
+    @Value("${db.driver.name}")
+    private String driverName;
+
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.min.idle}")
+    private Integer minIdle;
+
+    @Value("${db.max.idle}")
+    private Integer maxIdle;
+
+    @Bean
+    @Scope("singleton")
+    public DataSource mysqlDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driverName);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxIdle(maxIdle);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(mysqlDataSource());
+    }
+}
